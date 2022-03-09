76c02e6961252a41c11316d8fb2e64ede7d166d9
Dhruvin Moradiya <mdhruvin946@gmail.com>
2022-03-09 15:14:32 -0400

    cache-memory working

--- src/main/java/com/example/walnutAssessment/services/PostsService.java
+++ src/main/java/com/example/walnutAssessment/services/PostsService.java
@@ -3,19 +3,24 @@ package com.example.walnutAssessment.services;
 import com.example.walnutAssessment.DAO.PostDao;
 import com.example.walnutAssessment.models.Post;
 import com.example.walnutAssessment.utils.PostComparator;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.*;
 
 @Service
+@Slf4j
 public class PostsService {
 
     @Autowired
     private WebClient.Builder webClientBuilder;
 
+    @Cacheable(cacheNames="posts")
     public SortedSet<Post> getPosts(String[] tags, String sortBy, String direction) {
+        log.info("check");
         SortedSet<Post> posts = new TreeSet<>(new PostComparator(sortBy, direction));
 
         Arrays.stream(tags).forEach(tag -> {
